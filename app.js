c6d636e23faf8e3ad17450f9ec36ab2457f5c816
Himanshu <himanshu80921@gmail.com>
2024-08-03 16:46:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//NAVBAR SCRIPT---------------
+
+function toggleMenu(){
+    let menu = document.querySelector('.menu-links');
+    let icon = document.querySelector('.phn-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+};
+
+//NAVBAR SCRIPT----------------
+
